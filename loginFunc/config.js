fa9cb75557a42b6dcaf093d71f2cbad08e0235c2
hirokisatogit <49369555+hirokisatogit@users.noreply.github.com>
2021-01-20 00:29:29 +0900

    commit

---
+++ loginFunc/config.js
@@ -0,0 +1,14 @@
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyA_AmJiXG-UFTrUfxBFGg6XPq56Yx5gZPg",
+    authDomain: "image-guardian.firebaseapp.com",
+    projectId: "image-guardian",
+    storageBucket: "image-guardian.appspot.com",
+    messagingSenderId: "1098457553177",
+    appId: "1:1098457553177:web:4eb18337b72b6f05a54d88",
+    measurementId: "G-TJHNP81CTF"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
